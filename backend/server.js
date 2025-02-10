9e85906a7d469c107dad0262a919287104e58fbe
Josue Baldeon Araujo <136621348+Klell11@users.noreply.github.com>
2025-02-10 14:23:27 -0500

    lanzamiento

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 // backend/server.js
 require('dotenv').config();
+const path = require('path'); // Importa el módulo 'path'
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-// Importa tus otras rutas:
+// Importa tus rutas
 const carreterasRoutes = require('./routes/carreteras');
 const constructorasRoutes = require('./routes/constructoras');
 const presupuestosRoutes = require('./routes/presupuestos');
-
-// Importa la nueva ruta de proyectos
 const proyectosRoutes = require('./routes/proyectos');
 
 const app = express();
@@ -18,15 +17,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Rutas
+// Rutas de la API
 app.use('/api/carreteras', carreterasRoutes);
 app.use('/api/constructoras', constructorasRoutes);
 app.use('/api/presupuestos', presupuestosRoutes);
-
-// Nueva ruta para Proyectos
 app.use('/api/proyectos', proyectosRoutes);
 
+// Servir archivos estáticos del frontend (solo en producción)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
+  // Ruta para manejar todas las solicitudes y servir el archivo index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  });
+}
+
+// Conexión a MongoDB
 const PORT = process.env.PORT || 4000;
 const URI = process.env.MONGODB_URI; // Verifica que esta variable no sea undefined
 
@@ -42,6 +49,7 @@ mongoose.connect(URI, {
 .then(() => console.log("✅ Conectado a MongoDB Atlas"))
 .catch(err => console.error("❌ Error al conectar a MongoDB:", err));
 
+// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
 });
